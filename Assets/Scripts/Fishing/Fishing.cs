bcbba75907520fd71048fc18fdeb00f7119fd2c6
jesse <webbj19@ferris.edu>
2020-06-12 13:50:32 -0400

    updated fish

---
+++ Assets/Scripts/Fishing/Fishing.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fishing : MonoBehaviour
+{
+    public FishDataCollection fish;
+    List <FishDataObject> catchable = new List<FishDataObject>();
+    string fishDataCollectionAttraction;
+    string currentFishAttraction;
+
+    public Dropdown attractantOptions;
+    public Dropdown RetrivalMethodOptions;
+    public Dropdown CastingRangeOptions;
+    public Dropdown TimeOfDayOptions;
+    public Dropdown BodyOfWaterTypeOptions;
+    public Dropdown toolRequiredOptions;
+    public Dropdown enticeMethdodOptions;
+
+
+
+    enum currentAttractant
+    {  Lure, Living }
+    enum currentRetrivalMethdod
+    { Instant, Constant, On,Off }
+    enum currentCastingRange
+    { Close, Far }
+    enum currentTimeOfDay
+    { Morning, Day, Evening, Night }
+    enum currentBodyOfWaterType
+    { Morning, Day, Evening, Night }
+    enum currentToolRequiredOptions
+    { None, Any, Rod, Spear }
+    enum currententiceMethdod
+    {
+        Rhythmic, Random, Predictive
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       // print(catchable[0]);
+        DropDownOptions();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  print(attractantOptions.value);
+        fishDataCollectionAttraction = fish.FishDataObjects[1].Attractant[0].instanceOfEnum.ToString();
+        SetCurrentFishingMethdod();
+        print(currentFishAttraction);
+        /*
+        if (fishy == fishyss)
+        {
+            print("caught yeah");
+            catchable.Add(fish.FishDataObjects[1]);
+        }
+        print((currentAttractant)attractantOptions.value);
+
+        print((currentAttractant)attractantOptions.value);
+        */
+    }
+
+    private void SetCurrentFishingMethdod()
+    {
+        if (currentAttractant.Lure == (currentAttractant)attractantOptions.value)
+        {
+            currentFishAttraction = currentAttractant.Lure.ToString();
+        }
+        if (currentAttractant.Living == (currentAttractant)attractantOptions.value)
+        {
+            currentFishAttraction = currentAttractant.Living.ToString();
+        }
+    }
+
+    void DropDownOptions()
+    {
+        // sets attraction dropdown
+        string[] enumAttractant = Enum.GetNames(typeof(currentAttractant));
+        List<string> currentAttractantDDOptions = new List<string>(enumAttractant);
+        attractantOptions.AddOptions(currentAttractantDDOptions);
+        // sets retrival dropdown
+
+        string[] enumRetrivalMethodOptions = Enum.GetNames(typeof(currentRetrivalMethdod));
+        List<string> currentRetrivalMethdodDDOptions = new List<string>(enumRetrivalMethodOptions);
+        RetrivalMethodOptions.AddOptions(currentRetrivalMethdodDDOptions);
+        // sets casting range methdod
+        string[] enumCastingRangeOptions = Enum.GetNames(typeof(currentCastingRange));
+        List<string> enumCastingRangeDDOptions = new List<string>(enumCastingRangeOptions);
+        RetrivalMethodOptions.AddOptions(enumCastingRangeDDOptions);
+        //sets time of day
+        string[] enumCastingRangeOptions = Enum.GetNames(typeof(currentCastingRangeOptions));
+        List<string> enumCastingRangeDDOptions = new List<string>(enumCastingRangeOptions);
+        RetrivalMethodOptions.AddOptions(enumCastingRangeDDOptions);
+
+    }
+}
